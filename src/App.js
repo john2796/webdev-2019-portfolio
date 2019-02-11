c5c1da743614501e95bc5ab200a1ae447b12c67d
john2796 <jbmiranda22796@gmail.com>
2019-02-10 21:15:01 -0800

    added folder structure and homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <p>Home</p>
+        {/* sticky */}
+        <p>Navbar</p>
+        <p>About</p>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contacts</p>
+      </React.Fragment>
     );
   }
 }
