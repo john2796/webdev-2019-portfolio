a30cbbca06c60ac09433d29b31883eb7fd50424f
john2796 <jbmiranda22796@gmail.com>
2019-04-16 15:09:41 -0700

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,35 +21,35 @@ class Home extends Component {
   render() {
     return (
       <Element name="Home">
-        <HomeContainer>
-          <Particles />
-          <HeaderContainer>
-            <TextContainer>
-              <NameText>
-                Hello, I'm
-                <span> John Benedict Miranda.</span>
-                <br /> I'm a full stack web developer
-              </NameText>
-            </TextContainer>
-            <NavButton
-              name="Explore"
-              color="#c0c0c0"
-              border="2px solid #c0c0c0"
-              bgColor="#131928"
-              hoverFontColor="#FEFFFE"
-              borderColor="#E5FCF5"
-              onClick={this.smoothScroll}
-              fontSize="1.2rem"
-              width="160px"
-              mobileFontSize="1.2rem"
-              mobileWidth="140px"
-              mobileHeight="40px"
-            />
-          </HeaderContainer>
-        </HomeContainer>
+      <HomeContainer>
+      <Particles />
+      <HeaderContainer>
+      <TextContainer>
+      <NameText>
+      Hello, I'm
+      <span> John Benedict Miranda .</span>
+      <br /> I'm a full stack web developer
+      </NameText>
+      </TextContainer>
+      <NavButton
+      name="Explore"
+      color="#c0c0c0"
+      border="2px solid #c0c0c0"
+      bgColor="#131928"
+      hoverFontColor="#FEFFFE"
+      borderColor="#E5FCF5"
+      onClick={this.smoothScroll}
+      fontSize="1.2rem"
+      width="160px"
+      mobileFontSize="1.2rem"
+      mobileWidth="140px"
+      mobileHeight="40px"
+      />
+      </HeaderContainer>
+      </HomeContainer>
       </Element>
-    );
+      );
+    }
   }
-}
 
-export default Home;
+  export default Home;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -50,312 +50,312 @@ class Projects extends Component {
     const { one, two, three, four, five, seven } = this.state;
     return (
       <Element name="Projects">
-        <ProjectContainer>
-          <ContentWrapper>
-            <ProjectSubHeader>
-              PROJECTS
-              <div className="header-bar" />
-            </ProjectSubHeader>
+      <ProjectContainer>
+      <ContentWrapper>
+      <ProjectSubHeader>
+      PROJECTS
+      <div className="header-bar" />
+      </ProjectSubHeader>
 
-            <ThumbnailContainer>
-              <ProjectSection>
-                <Thumbnail
-                  src={img7}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("seven")}
-                  alt="project card"
-                />
+      <ThumbnailContainer>
+      <ProjectSection>
+      <Thumbnail
+      src={img7}
+      style={thumbSize}
+      onClick={this._toggleHandler("seven")}
+      alt="project card"
+      />
 
-                <Thumbnail
-                  src={img5}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("five")}
-                  alt="project card"
-                />
+      <Thumbnail
+      src={img5}
+      style={thumbSize}
+      onClick={this._toggleHandler("five")}
+      alt="project card"
+      />
 
-                <Thumbnail
-                  src={img3}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("three")}
-                  alt="project card"
-                />
+      <Thumbnail
+      src={img3}
+      style={thumbSize}
+      onClick={this._toggleHandler("three")}
+      alt="project card"
+      />
 
-                <Thumbnail
-                  src={img1}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("one")}
-                  alt="project card"
-                />
+      <Thumbnail
+      src={img1}
+      style={thumbSize}
+      onClick={this._toggleHandler("one")}
+      alt="project card"
+      />
 
-                <Thumbnail
-                  src={img2}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("two")}
-                  alt="project card"
-                />
+      <Thumbnail
+      src={img2}
+      style={thumbSize}
+      onClick={this._toggleHandler("two")}
+      alt="project card"
+      />
 
-                <Thumbnail
-                  src={img4}
-                  style={thumbSize}
-                  onClick={this._toggleHandler("four")}
-                  alt="project card"
-                />
-              </ProjectSection>
-            </ThumbnailContainer>
+      <Thumbnail
+      src={img4}
+      style={thumbSize}
+      onClick={this._toggleHandler("four")}
+      alt="project card"
+      />
+      </ProjectSection>
+      </ThumbnailContainer>
 
-            <ButtonWrapper>
-              <a
-                href="https://www.johnbenedictmiranda.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "none" }}
-              />
-            </ButtonWrapper>
-          </ContentWrapper>
-        </ProjectContainer>
+      <ButtonWrapper>
+      <a
+      href="https://www.johnbenedictmiranda.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ textDecoration: "none" }}
+      />
+      </ButtonWrapper>
+      </ContentWrapper>
+      </ProjectContainer>
 
-        {/* ------------------------------- one -------------------------------*/}
-        <Modal isOpen={one} toggle={this._toggleHandler("one")}>
-          <ModalBody>
-            <a
-              href="https://miranda-insta-clone.netlify.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize src={img1} style={fullSize} alt="A Instagram clone" />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>Instagram</ModalTitle>
-              <ModalSubTitle>A Instagram clone</ModalSubTitle>
-              <ModalDescription>
-                built with React, Redux, Node.js, Express, Mongoose, Bycrpyt ,
-                Passport , Passport-jwt.
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("one")}>
-            <Astyle
-              href="https://miranda-insta-clone.netlify.com"
-              target="_blank"
-            >
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>{" "}
-            <Astyle
-              href="https://github.com/john2796/React-Insta-Clone-1"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
-        {/* ------------------------------- two -------------------------------*/}
-        <Modal isOpen={two} toggle={this._toggleHandler("two")}>
-          <ModalBody>
-            <a
-              href="https://miranda-usemytools.netlify.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize src={img2} style={fullSize} alt="Use My Tool" />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>Use My Tool</ModalTitle>
-              <ModalSubTitle />
-              <ModalDescription>
-                It doesn't make sense to purchase many items that are costly.
-                Instead, you can rent such items from reliable people and
-                businesses in your local community using the Use My Tool app.
-                Built with React Bootstrap with Material Design && MERN
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("two")}>
-            <Astyle
-              href="https://miranda-usemytools.netlify.com"
-              target="_blank"
-            >
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>{" "}
-            <Astyle
-              href="https://github.com/use-my-tools/Front-End"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-            <Astyle
-              href="https://www.youtube.com/watch?v=7SomB_hSfS0&feature=youtu.be"
-              target="_blank"
-            >
-              <i className="fab fa-youtube fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
+    {/* ------------------------------- one -------------------------------*/}
+    <Modal isOpen={one} toggle={this._toggleHandler("one")}>
+    <ModalBody>
+    <a
+    href="https://miranda-insta-clone.netlify.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    >
+    <FullSize src={img1} style={fullSize} alt="A Instagram clone" />
+    </a>
+    </ModalBody>
+    <ModalHeader style={{ paddingTop: "0" }}>
+    <ModalTextWrapper>
+    <ModalTitle>Instagram</ModalTitle>
+    <ModalSubTitle>A Instagram clone</ModalSubTitle>
+    <ModalDescription>
+    built with React, Redux, Node.js, Express, Mongoose, Bycrpyt ,
+    Passport , Passport-jwt.
+    </ModalDescription>
+    </ModalTextWrapper>
+    </ModalHeader>
+    <ModalFooter toggle={this._toggleHandler("one")}>
+    <Astyle
+    href="https://miranda-insta-clone.netlify.com"
+    target="_blank"
+    >
+    <i className="fas fa-globe fa-2x" />
+    </Astyle>{" "}
+    <Astyle
+    href="https://github.com/john2796/React-Insta-Clone-1"
+    target="_blank"
+    >
+    <i className="fab fa-github fa-2x" />
+    </Astyle>
+    </ModalFooter>
+    </Modal>
+  {/* ------------------------------- two -------------------------------*/}
+  <Modal isOpen={two} toggle={this._toggleHandler("two")}>
+  <ModalBody>
+  <a
+  href="https://miranda-usemytools.netlify.com"
+  target="_blank"
+  rel="noopener noreferrer"
+  >
+  <FullSize src={img2} style={fullSize} alt="Use My Tool" />
+  </a>
+  </ModalBody>
+  <ModalHeader style={{ paddingTop: "0" }}>
+  <ModalTextWrapper>
+  <ModalTitle>Use My Tool</ModalTitle>
+  <ModalSubTitle />
+  <ModalDescription>
+  It doesn't make sense to purchase many items that are costly.
+  Instead, you can rent such items from reliable people and
+  businesses in your local community using the Use My Tool app.
+  Built with React Bootstrap with Material Design && MERN
+  </ModalDescription>
+  </ModalTextWrapper>
+  </ModalHeader>
+  <ModalFooter toggle={this._toggleHandler("two")}>
+  <Astyle
+  href="https://miranda-usemytools.netlify.com"
+  target="_blank"
+  >
+  <i className="fas fa-globe fa-2x" />
+  </Astyle>{" "}
+  <Astyle
+  href="https://github.com/use-my-tools/Front-End"
+  target="_blank"
+  >
+  <i className="fab fa-github fa-2x" />
+  </Astyle>
+  <Astyle
+  href="https://www.youtube.com/watch?v=7SomB_hSfS0&feature=youtu.be"
+  target="_blank"
+  >
+  <i className="fab fa-youtube fa-2x" />
+  </Astyle>
+  </ModalFooter>
+  </Modal>
 
-        {/* ------------------------------- three -------------------------------*/}
-        <Modal isOpen={three} toggle={this._toggleHandler("three")}>
-          <ModalBody>
-            <a
-              href="https://miranda-restaurant.netlify.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize
-                src={img3}
-                style={fullSize}
-                alt="The Soul Food & Bistro"
-              />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>The Soul Food & Bistro</ModalTitle>
-              <ModalSubTitle>A modern website for a Restaurant</ModalSubTitle>
-              <ModalDescription>
-                Miranda Restaurant is a site I created to showcase my passion
-                for food as well as my visual art. Built with React .
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("three")}>
-            <Astyle
-              href="https://miranda-restaurant.netlify.com"
-              target="_blank"
-            >
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>
-            <Astyle
-              href="https://github.com/john2796/portfolio-restaurant-01"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
+{/* ------------------------------- three -------------------------------*/}
+<Modal isOpen={three} toggle={this._toggleHandler("three")}>
+<ModalBody>
+<a
+href="https://miranda-restaurant.netlify.com"
+target="_blank"
+rel="noopener noreferrer"
+>
+<FullSize
+src={img3}
+style={fullSize}
+alt="The Soul Food & Bistro"
+/>
+</a>
+</ModalBody>
+<ModalHeader style={{ paddingTop: "0" }}>
+<ModalTextWrapper>
+<ModalTitle>The Soul Food & Bistro</ModalTitle>
+<ModalSubTitle>A modern website for a Restaurant</ModalSubTitle>
+<ModalDescription>
+Miranda Restaurant is a site I created to showcase my passion
+for food as well as my visual art. Built with React .
+  </ModalDescription>
+</ModalTextWrapper>
+</ModalHeader>
+<ModalFooter toggle={this._toggleHandler("three")}>
+<Astyle
+href="https://miranda-restaurant.netlify.com"
+target="_blank"
+>
+<i className="fas fa-globe fa-2x" />
+</Astyle>
+<Astyle
+href="https://github.com/john2796/portfolio-restaurant-01"
+target="_blank"
+>
+<i className="fab fa-github fa-2x" />
+</Astyle>
+</ModalFooter>
+</Modal>
 
-        {/* ------------------------------- four -------------------------------*/}
-        <Modal isOpen={four} toggle={this._toggleHandler("four")}>
-          <ModalBody>
-            <a
-              href="https://redux-nasa-fwmiybrxm.now.sh/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize src={img4} style={fullSize} alt="NASA" />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>NASA</ModalTitle>
-              <ModalDescription>
-                Built with NASA api and React.js
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("four")}>
-            <Astyle href="https://redux-nasa-fwmiybrxm.now.sh/" target="_blank">
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>{" "}
-            <Astyle
-              href="https://github.com/john2796/redux-nasa-api"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
+{/* ------------------------------- four -------------------------------*/}
+<Modal isOpen={four} toggle={this._toggleHandler("four")}>
+<ModalBody>
+<a
+href="https://redux-nasa-fwmiybrxm.now.sh/"
+target="_blank"
+rel="noopener noreferrer"
+>
+<FullSize src={img4} style={fullSize} alt="NASA" />
+</a>
+</ModalBody>
+<ModalHeader style={{ paddingTop: "0" }}>
+<ModalTextWrapper>
+<ModalTitle>NASA</ModalTitle>
+<ModalDescription>
+Built with NASA api and React.js
+</ModalDescription>
+</ModalTextWrapper>
+</ModalHeader>
+<ModalFooter toggle={this._toggleHandler("four")}>
+<Astyle href="https://redux-nasa-fwmiybrxm.now.sh/" target="_blank">
+<i className="fas fa-globe fa-2x" />
+</Astyle>{" "}
+<Astyle
+href="https://github.com/john2796/redux-nasa-api"
+target="_blank"
+>
+<i className="fab fa-github fa-2x" />
+</Astyle>
+</ModalFooter>
+</Modal>
 
-        {/* ------------------------------- five -------------------------------*/}
-        <Modal isOpen={five} toggle={this._toggleHandler("five")}>
-          <ModalBody>
-            <a
-              href="https://miranda-realestate.netlify.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize src={img5} style={fullSize} alt="miranda-realestate" />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>Los Angeles Experts</ModalTitle>
-              <ModalSubTitle />
-              <ModalDescription>
-                Built with: React.js , google-map-react, node-sass, reacstrap ,
-                bootstrap, redux, redux-thunk, styled-components,
-                react-router-dom
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("five")}>
-            <Astyle
-              href="https://miranda-realestate.netlify.com"
-              target="_blank"
-            >
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>{" "}
-            <Astyle
-              href="https://github.com/john2796/portfolio-003-real-estate"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
+{/* ------------------------------- five -------------------------------*/}
+<Modal isOpen={five} toggle={this._toggleHandler("five")}>
+<ModalBody>
+<a
+href="https://miranda-realestate.netlify.com"
+target="_blank"
+rel="noopener noreferrer"
+>
+<FullSize src={img5} style={fullSize} alt="miranda-realestate" />
+</a>
+</ModalBody>
+<ModalHeader style={{ paddingTop: "0" }}>
+<ModalTextWrapper>
+<ModalTitle>Los Angeles Experts</ModalTitle>
+<ModalSubTitle />
+<ModalDescription>
+Built with: React.js , google-map-react, node-sass, reacstrap ,
+bootstrap, redux, redux-thunk, styled-components,
+react-router-dom
+</ModalDescription>
+</ModalTextWrapper>
+</ModalHeader>
+<ModalFooter toggle={this._toggleHandler("five")}>
+<Astyle
+href="https://miranda-realestate.netlify.com"
+target="_blank"
+>
+<i className="fas fa-globe fa-2x" />
+</Astyle>{" "}
+<Astyle
+href="https://github.com/john2796/portfolio-003-real-estate"
+target="_blank"
+>
+<i className="fab fa-github fa-2x" />
+</Astyle>
+</ModalFooter>
+</Modal>
 
-        {/* ------------------------------- seven -------------------------------*/}
-        <Modal isOpen={seven} toggle={this._toggleHandler("seven")}>
-          <ModalBody>
-            <a
-              href="https://moviedb19.netlify.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FullSize src={img7} style={fullSize} alt="movieDB19" />
-            </a>
-          </ModalBody>
-          <ModalHeader style={{ paddingTop: "0" }}>
-            <ModalTextWrapper>
-              <ModalTitle>MovieDb19</ModalTitle>
-              <ModalSubTitle />
-              <ModalDescription>
-                <ul>
-                  <li>
-                    Built with : <br />- themoviedb api , react.js, redux,
-                    wow.js.
-                  </li>
-                  <li>
-                    description: <br />- the best movie app references for
-                    movies, actors, directors, and TV series . included are
-                    photos, biographies, trailers, descriptions, reviews, movie
-                    showtimes, TV listings, and more.
-                  </li>
-                  <li>
-                    Awesome company: <br />- this project is still under
-                    development 👾
-                  </li>
-                </ul>
-              </ModalDescription>
-            </ModalTextWrapper>
-          </ModalHeader>
-          <ModalFooter toggle={this._toggleHandler("seven")}>
-            <Astyle href="https://moviedb19.netlify.com/" target="_blank">
-              <i className="fas fa-globe fa-2x" />
-            </Astyle>{" "}
-            <Astyle
-              href="https://github.com/john2796/moviedb19"
-              target="_blank"
-            >
-              <i className="fab fa-github fa-2x" />
-            </Astyle>
-          </ModalFooter>
-        </Modal>
-      </Element>
-    );
-  }
+{/* ------------------------------- seven -------------------------------*/}
+<Modal isOpen={seven} toggle={this._toggleHandler("seven")}>
+<ModalBody>
+<a
+href="https://moviedb19.netlify.com/"
+target="_blank"
+rel="noopener noreferrer"
+>
+<FullSize src={img7} style={fullSize} alt="movieDB19" />
+</a>
+</ModalBody>
+<ModalHeader style={{ paddingTop: "0" }}>
+<ModalTextWrapper>
+<ModalTitle>MovieDb19</ModalTitle>
+<ModalSubTitle />
+<ModalDescription>
+<ul>
+<li>
+Built with : <br />- themoviedb api , react.js, redux,
+wow.js.
+</li>
+<li>
+description: <br />- the best movie app references for
+movies, actors, directors, and TV series . included are
+photos, biographies, trailers, descriptions, reviews, movie
+showtimes, TV listings, and more.
+</li>
+<li>
+Awesome company: <br />- this project is still under
+development 👾
+</li>
+</ul>
+</ModalDescription>
+</ModalTextWrapper>
+</ModalHeader>
+<ModalFooter toggle={this._toggleHandler("seven")}>
+<Astyle href="https://moviedb19.netlify.com/" target="_blank">
+<i className="fas fa-globe fa-2x" />
+</Astyle>{" "}
+<Astyle
+href="https://github.com/john2796/moviedb19"
+target="_blank"
+>
+<i className="fab fa-github fa-2x" />
+</Astyle>
+</ModalFooter>
+</Modal>
+</Element>
+);
+}
 }
 
 export default Projects;
